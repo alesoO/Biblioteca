95aef73370ad5161250c16840ef4ae187178bb66
Alexandre Soares <xxalesoaresxx@gmail.com>
2023-08-22 11:46:36 -0300

    Redução de codigo 	modified:   app/Http/Controllers/AuthorController.php 	modified:   app/Http/Controllers/BookController.php 	modified:   app/Http/Controllers/HomeController.php 	modified:   app/Http/Controllers/ProfileController.php 	modified:   app/Http/Controllers/PublisherController.php 	modified:   app/Http/Controllers/StudentController.php 	modified:   app/Http/Controllers/UserController.php

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -10,21 +10,15 @@ use Illuminate\Support\Facades\Validator;
 
 class BookController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         $books = Book::paginate(15);
-        
+
         $authors = Author::all();
         $publishers = Publisher::all();
         return view('table_book', compact('books', 'authors', 'publishers'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create(Request $request)
     {
         $validator = Validator::make($request->all(), [
@@ -45,7 +39,6 @@ class BookController extends Controller
             'publisher_id' => $request->input('publisher_id')
         ]);
 
-
         $fieldValues['title'] = strip_tags($fieldValues['title']);
         $fieldValues['page'] = strip_tags($fieldValues['page']);
 
@@ -58,30 +51,6 @@ class BookController extends Controller
         return redirect('/table_book')->with('sucess');
     }
 
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, Book $book)
     {
         $validator = Validator::make($request->all(), [
@@ -102,7 +71,6 @@ class BookController extends Controller
             'publisher_id' => $request->input('publisher_id')
         ]);
 
-
         $fieldValues['title'] = strip_tags($fieldValues['title']);
         $fieldValues['page'] = strip_tags($fieldValues['page']);
 
@@ -115,9 +83,6 @@ class BookController extends Controller
         return redirect('/table_book');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Book $book)
     {
         try {
